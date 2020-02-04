5b07fe1c482a47b39c102d0ccbd7bcf5ac1cc963
Chloe Gell <gellert.chloe@gmail.com>
2020-02-04 18:20:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,9 +25,11 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end 
   end 
-  def reverse_transfer 
-    @sender.balance += amount 
-    @receiver.balance -= amount 
-    @status = "reversed"
+  def reverse_transfer
+    if(@status == "complete")
+      @sender.balance += amount 
+      @receiver.balance -= amount 
+      @status = "reversed"
+    end 
   end 
 end
